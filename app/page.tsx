0af0ac36a80943a5115ee282c1ee1c45b8a8211c
Franco Maciel <damianmaciel0@gmail.com>
2024-10-03 21:12:07 -0300

    add new products

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,13 @@ import MetadataJSON from '@/data/metadata.json'
 import ContactButton from "@/components/button/contact"
 import type { Metadata } from "next"
 import BoxReveal from "@/components/magicui/box-reveal"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export const metadata: Metadata = {
   metadataBase: new URL(MetadataJSON.url),
@@ -71,7 +78,7 @@ export default function ProductLandingPage() {
         <section className="w-full p-10 m-auto bg-muted" id="products">
           <div className="m-auto container">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-8 text-neutral-800">Productos</h2>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 grid-cols-1">
+            {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 grid-cols-1">
               {ProductsJSON.products.map((product) => (
                 <ProductCard
                   id={product.id}
@@ -84,7 +91,34 @@ export default function ProductLandingPage() {
                   price={product.price}
                 />
               ))}
-            </div>
+            </div> */}
+            <Carousel
+              opts={{
+                align: "start",
+              }}
+              className="w-full"
+            >
+              <CarouselContent>
+                {ProductsJSON.products.map((product) => (
+                  <CarouselItem key={product.id} className="md:basis-1/2 xl:basis-1/4 lg:basis-1/3">
+                    <div className="p-4">
+                      <ProductCard
+                        id={product.id}
+                        key={product.id}
+                        title={product.name}
+                        image={{
+                          src: product.img[0],
+                          alt: `${product.name} - ${product.id}`
+                        }}
+                        price={product.price}
+                      />
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
           </div>
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32 bg-neutral-950" id="about">
