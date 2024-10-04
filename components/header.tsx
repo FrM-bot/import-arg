0bc42e69a7b873da0877ba34b1e0dcb3b0c971ce
Franco Maciel <damianmaciel0@gmail.com>
2024-10-03 22:20:34 -0300

    add mobile navar

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,13 @@ import { routes } from '@/lib/routes'
 import Link from 'next/link'
 import React from 'react'
 import { FadeText } from './magicui/fade-text'
+import { Nav } from './nav'
+import { MenuMobile } from './menu.mobile'
 
 export default function Header() {
     return (
-        <header className="px-4 lg:px-6 sm:h-14 py-4 flex items-center justify-center fixed top-0 bg-black/90 backdrop-blur-md text-neutral-200 z-50 w-full">
-            <div className='container flex flex-col sm:flex-row sm:items-center items-center gap-2'>
+        <header className="sm:h-14 py-4 flex items-center justify-center fixed top-0 bg-black/90 backdrop-blur-md text-neutral-200 z-50 w-full">
+            <div className='container flex sm:items-center items-center gap-2 justify-between'>
                 <Link href={routes.home}>
                     <FadeText
                         className="max-w-[600px] md:text-xl text-lg"
@@ -17,17 +19,8 @@ export default function Header() {
                         text="ImportARG"
                     />
                 </Link>
-                <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-                    <Link className="text-sm font-medium hover:underline underline-offset-4" href={`${routes.home}#products`}>
-                        Productos
-                    </Link>
-                    <Link className="text-sm font-medium hover:underline underline-offset-4" href={`${routes.home}#about`}>
-                        Sobre nosotros
-                    </Link>
-                    <Link className="text-sm font-medium hover:underline underline-offset-4" href={`${routes.home}#contact`}>
-                        Contáctenos
-                    </Link>
-                </nav>
+                <Nav />
+                <MenuMobile />
             </div>
         </header>
     )
