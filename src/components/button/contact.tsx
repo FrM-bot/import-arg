faa8161ccdcb05a8d3c45a6f25001cdb2649063f
Franco Maciel <damianmaciel0@gmail.com>
2025-06-11 19:06:23 -0300

    feat: add WhatsApp contact button component with fixed positioning option

--- src/components/button/contact.tsx
+++ src/components/button/contact.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import { WhatsApp } from "../icons";
 import { cn, getWhatsAppUrl } from "@/lib/utils";
@@ -13,7 +14,7 @@ export function ContactButton({
     <a
       className={cn(
         buttonVariants({ size: "icon", variant: "outline" }),
-        "h-12 w-12 relative bg-white/50 backdrop-blur-sm text-black"
+        "h-12 w-12 relative bg-white/90 backdrop-blur-sm text-black"
       )}
       href={getWhatsAppUrl({
         text: text || "Hola, me gustaría consultarte acerca de un producto",
@@ -25,14 +26,14 @@ export function ContactButton({
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-neutral-300 group-hover:rotate-45 duration-150" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-neutral-300 group-hover:rotate-45 duration-150" />
       <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-neutral-300 group-hover:rotate-45 duration-150" />
-      <WhatsApp />
+      <WhatsApp className="text-[#25d366]" />
     </a>
   );
 }
 
 export function FixedContactButton() {
   return (
-    <div className="sticky bottom-16 left-[90%] bg-transparent w-fit h-fit p-6">
+    <div className="sticky bottom-16 left-[90%] w-fit h-fit p-6">
       <ContactButton />
     </div>
   );
